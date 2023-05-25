3191809989cb0ec2e5d92ab55cd6fdbfe14a9daf
DavidX787 <340868082@gapps.yrdsb.ca>
2023-05-25 09:20:03 -0400

    Animated Elephant

--- Elephant.java
+++ Elephant.java
@@ -9,6 +9,23 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Elephant extends Actor
 {
     GreenfootSound elephantSound = new GreenfootSound("elephantcub.mp3");
+    GreenfootImage[] idle = new GreenfootImage[8];
+    public Elephant()
+    {
+        for(int i = 0; i < idle.length; i ++)
+        {
+            idle[i] = new GreenfootImage("images/elephant_idle/idle" + i + ".png");
+        }
+        setImage(idle[0]);
+    }
+    
+    int imageIndex = 0;
+    public void animateElephant()
+    {
+        setImage(idle[imageIndex]);
+        imageIndex = (imageIndex + 1) % idle.length;
+    }
+    
     /**
      * Act - do whatever the Elephant wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
@@ -25,6 +42,7 @@ public class Elephant extends Actor
         }
         
         eat();
+        animateElephant();
     }    
     //if eats apple, makes new apple 
     public void eat()
